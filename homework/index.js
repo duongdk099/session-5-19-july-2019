c74be6ce14220046ccca9708a40f75de733c58fb
duongdk099 <52460163+duongdk099@users.noreply.github.com>
2019-07-23 18:20:52 +0700

    Add files via upload

---
+++ homework/index.js
@@ -0,0 +1,65 @@
+// turtle exercice
+
+// Write a function that prints out “Hello world” 3 times (note: no arguments, no return)
+// let HelloWorld = () =>
+// {
+//     for ( let i=1 ; i <=3 ; i++)
+//     {
+//     console.log("Hello world");
+//     }
+// }
+// HelloWorld();
+
+// Write a function that takes 2 numbers as arguments and print out sum of them (note: has arguments, no return)
+// let addSum = (x,y) =>
+// {
+//     console.log(`${x+y}`);
+// }
+// addSum(5,6);
+
+
+
+// write a function remove ("$") in a string , named : remove_dollar_sign
+// let s = "hallo$";
+// let remove_dollar_sign = (s) =>
+// {       
+    
+//     s = s.split("$").join("");
+//     return s;
+    
+// }
+
+// string_with_no_dollars = remove_dollar_sign("$80% percent of $life is to show $up")
+// if (string_with_no_dollars == "80% percent of life is to show up"){
+//     console.log("Your function is correct")}
+// else{
+//     console.log("Oops, there's a bug")}
+
+//Write a function that extracts the even items in a given integer list, named get_even_list
+
+// let get_even_list = (list) =>
+// {
+//     let realList=[];
+//     for ( let i=0; i<list.length;i++)
+//     {
+//         if ( list[i] %2 == 0)
+//         {
+//             realList.push(list[i]);
+//         }
+//     }
+//     console.log(realList);
+    
+//     return realList.toString();
+// }
+
+// // get_even_list ([1,2,3,4,1,2,3,1,2]);
+
+// even_list = get_even_list([1, 2, 5, 9, 10, 6])
+
+// if (even_list == [2, 10, 6]){
+//     console.log("Your function is correct")}
+// else 
+// {
+//     console.log("Ooops, bugs detected")}
+
+
